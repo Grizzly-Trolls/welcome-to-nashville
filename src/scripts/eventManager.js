6742f5b9d3a1642de3f9ca39138eb907a9fa0bfe
joseph walker <josephwalker504@gmail.com>
2019-10-22 11:39:34 -0500

    Added code is working finally

--- src/scripts/eventManager.js
+++ src/scripts/eventManager.js
@@ -11,9 +11,21 @@
 
 
 // ***** RESTAURANTS (Joseph) ***** //
+const restaurantHandleSearch = event => {
+    const inputFeild = document.querySelector("input")
+    console.log("user input", inputFeild.value)
 
+    searchRestaurants(inputFeild.value)
+    .then(response => {
+        displayRestaurantNameHTML(respone.names)
+        inputFeild.value =""
+    })
+}
 
-
+const attachEventListenerToSearchButton = () => {
+    const searchButton = document.getElementById("search-button")
+    searchButton.addEventListener("click", restaurantHandleSearch)
+}
 
 
 // ***** CONCERTS (Cassie) ***** //
