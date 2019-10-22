7d5f4ab72f5a7b8cc0fb8bf00c48fa0cd65a9b65
Schatfield <dgoldmine@icloud.com>
2019-10-22 10:57:40 -0500

    added refernce to DOM for search form and made vaiables to hold API info

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,8 +1,15 @@
 // ***** PARKS (Shawna) ***** //
 
 
+const parksBaseUrl = "https://data.nashville.gov/resource/"
 
-
+const searchParks = (dropDownItem) => {
+    return fetch (`${parksBaseUrl}74d7-b74t.json?${parksAppToken}&${dropDownItem}`)
+}
+// const searchRecipes = (searchInput) => { 
+    //     return fetch(`${apiBaseUrl}search?key=${food2ForkKey}&q=${searchInput}`).then(response => response.json());
+    // }
+    
 
 // ***** MEETUPS (Caroline) ***** //
 
@@ -17,3 +24,4 @@
 
 
 // ***** CONCERTS (Cassie) ***** //
+
