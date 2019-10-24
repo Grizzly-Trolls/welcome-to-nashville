f05629ad00197f397b51fd5d1371ff8f30db9f05
Caroline Brownlee <brownleecaroline@gmail.com>
2019-10-24 13:44:45 -0500

    fixed bugs with merging cassie's branch to master and master to this branch. All code works together. Ready to push and create a pull request.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,15 +20,13 @@ buildRestaurantSearchForm()
 
 // Function call for building and appending search form, concerts section (searchFormManager.js). Makes button and search field.
 concertsBuildAndAppendSearchForm()
-eventbriteSearchButton() 
-attachEventListenerToRestaurantSearchButton()
-eventListenerToRestaurantSaveButton()
 
 
 // ******* ATTACHING EVENT LISTENERS *****
 
 
 // Function call to attach event listener to concert search button (eventManager.js)
+eventbriteSearchButton()
 attachEventListenerToConcertsSearchButton()
 // eventListenerToRestaurantSaveButton()
 attachEventListenerToRestaurantSearchButton()
