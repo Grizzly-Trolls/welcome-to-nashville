cd84a70ba3bfa9fdeb58bc7fa18aba0912046ed2
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-23 21:33:31 -0500

    Added functions to get console logs of api info

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+
 // ***** PARKS (Shawna) ***** //
 
 
@@ -18,4 +19,13 @@ buildRestaurantSearchForm()
 
 
 // ***** CONCERTS (Cassie) ***** //
+
+// Function call for building and appending search form, concerts section (searchFormManager.js). Makes button and search field.
 concertsBuildAndAppendSearchForm()
+
+
+// ******* ATTACHING EVENT LISTENERS *****
+
+
+// Function call to attach event listener to concert search button (eventManager.js)
+attachEventListenerToConcertsSearchButton()
--- src/scripts/searchResultsManager.js
+++ src/scripts/searchResultsManager.js
@@ -37,3 +37,36 @@ const displayEventbriteHtml = events => {
 
 
 // ***** CONCERTS (Cassie) ***** //
+
+// Function definition that adds a HTML string to the DOM. concerts is an argument.
+const buildConcertsHtml = concerts => `
+<article>
+  <h4>${concerts.title}</h4>
+  <p>
+      Info: ${concerts.address}
+  </p>
+</article>
+`
+
+// Function definition that takes concerts (fetch_call_response.concerts) as an argument. Called on the eventManager.js page
+const displayConcertsHtml = allConcerts => {
+
+  // Declare concertsResultsHtml as an empty string.
+  let concertsResultsHtml = ""
+
+  // .forEach loop loops through allConcerts (response.recipes) array.
+  allConcerts.forEach(concert => {
+
+    // Variable concertHtml is assigned to the value returned from calling the function buildConcertsHtml.
+    let concertHtml = buildConcertsHtml(concert)
+
+    // recipeHtml is added to the existing value of recipeResultsHtml.
+    concertsResultsHtml += recipeHtml
+  });
+
+  // The DOM is searched for the first class named .search-results and assigns it to a variable.
+  const searchResultsContainer = document.querySelector("#results-container")
+
+  // The innerHTML (area between tageted HTML tags) of the searchResultsSection is populated with the value of recipeResultsHtml.
+  searchResultsSection.innerHTML = recipeResultsHtml
+}
